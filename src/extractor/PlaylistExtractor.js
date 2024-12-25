74ff25c7e6bf8ca1e56161c858c98c4d94f8d331
SolareFlame <zeldac332@gmail.com>
2024-12-25 20:51:38 +0100

    🏗️ No more a class + static but just function + logs

--- src/extractor/PlaylistExtractor.js
+++ src/extractor/PlaylistExtractor.js
@@ -1,46 +1,45 @@
 const Song = require('../utils/Song');
+const {spawn} = require("node:child_process");
 
-class PlaylistExtractor {
-    /**
-     * Extrait les URLs de vidéos d'une playlist YouTube.
-     * @returns {Promise<string[]>} Une promesse contenant une liste d'URLs de vidéos.
-     * @param url
-     */
-    static async getPlaylistURLs(url) {
-        return new Promise((resolve, reject) => {
-            const process = spawn('yt-dlp', ['--flat-playlist', '-J', url]);
+/**
+ * Extrait les URLs de vidéos d'une playlist YouTube.
+ * @returns {Promise<string[]>} Une promesse contenant une liste d'URLs de vidéos.
+ * @param url
+ */
+async function getPlaylistURLs(url) {
+    return new Promise((resolve, reject) => {
+        const process = spawn('yt-dlp', ['--flat-playlist', '-J', url]);
 
-            let output = '';
-            let error = '';
+        let output = '';
+        let error = '';
 
-            process.stdout.on('data', (data) => {
-                output += data.toString();
-            });
-
-            process.stderr.on('data', (data) => {
-                error += data.toString();
-            });
-
-            process.on('close', (code) => {
-                if (code === 0) {
-                    try {
-                        const jsonOutput = JSON.parse(output);
-                        const videoUrls = jsonOutput.entries.map(
-                            (entry) => `https://www.youtube.com/watch?v=${entry.id}`
-                        );
-                        resolve(videoUrls);
-                    } catch (err) {
-                        reject(`Erreur lors de l'analyse des données : ${err.message}`);
-                    }
-                } else {
-                    reject(`Erreur yt-dlp : ${error}`);
-                }
-            });
+        process.stdout.on('data', (data) => {
+            output += data.toString();
         });
-    }
+
+        process.stderr.on('data', (data) => {
+            error += data.toString();
+        });
+
+        process.on('close', (code) => {
+            if (code === 0) {
+                try {
+                    const jsonOutput = JSON.parse(output);
+                    const videoUrls = jsonOutput.entries.map(
+                        (entry) => `https://www.youtube.com/watch?v=${entry.id}`
+                    );
+                    resolve(videoUrls);
+                } catch (err) {
+                    reject(`[PLAYLIST EXT] : ${err.message}`);
+                }
+            } else {
+                reject(`[PLAYLIST EXT] : ${error}`);
+            }
+        });
+    });
 }
 
-module.exports = PlaylistExtractor;
+module.exports = {getPlaylistURLs};
 
 
 
