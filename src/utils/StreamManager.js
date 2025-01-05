6b549b92c1ba5bb6f52d075373a3dc03c8fb02bc
SolareFlame <zeldac332@gmail.com>
2025-01-05 19:56:04 +0100

    🚀 StreamMangager (old AudioManager) that use Stream as a ressource

---
+++ src/utils/StreamManager.js
@@ -0,0 +1,69 @@
+const { PassThrough } = require('stream');
+const { createAudioResource, StreamType } = require('@discordjs/voice');
+const { spawn } = require('child_process');
+
+/**
+ * @param url
+ */
+async function createAudioStream(url){
+    try {
+        console.log(`[AUDIOSTREAM] : New audio stream created from ${url}`);
+
+        const audioStream = new PassThrough();
+        const ytDlpProcess = spawn('yt-dlp', [
+            '-o',
+            '-',
+            '-f',
+            'bestaudio[ext=webm]',
+            url
+        ]);
+
+        ytDlpProcess.stderr.on('data', (data) => {
+            console.error(`[yt-dlp] : ${data.toString().trim()}`);
+        });
+
+        ytDlpProcess.on('close', (code) => {
+            if (code !== 0) {
+                console.error(`[yt-dlp] edded with code ${code}`);
+                audioStream.end();
+            }
+        });
+
+
+        const ffmpegProcess = spawn('ffmpeg', [
+            '-i', 'pipe:0',
+            '-c:a', 'libopus',
+            '-b:a', '128k',
+            '-f', 'webm',
+            'pipe:1',
+        ]);
+
+        ytDlpProcess.stdout.pipe(ffmpegProcess.stdin);
+        ffmpegProcess.stdout.pipe(audioStream);
+
+        ffmpegProcess.stderr.on('data', (data) => {
+            console.error(`[ffmpeg] : ${data.toString().trim()}`);
+        });
+
+        ffmpegProcess.on('close', (code) => {
+            if (code !== 0) {
+                console.error(`[ffmpeg] ended with code ${code}`);
+                audioStream.end();
+            }
+        });
+
+        const resource = createAudioResource(audioStream, {
+            metadata: undefined,
+            inputType: StreamType.WebmOpus
+        });
+
+        console.log('[AUDIOSTREAM] : Audio stream created successfully');
+        return resource;
+
+    } catch (error) {
+        console.error('[AUDIOSTREAM] : Error creating audio stream');
+        throw error;
+    }
+}
+
+module.exports = { createAudioStream };
