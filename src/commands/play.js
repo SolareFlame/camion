73e8c42f61af6a199713b72efc982c87c857bc96
SolareFlame <zeldac332@gmail.com>
2025-01-24 02:01:09 +0100

    🚀 Deploy new playlist load to queue (Faster asf)

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,6 +4,7 @@ const Song = require("../utils/Song");
 const EmbedManager = require("../embed/EmbedManager");
 const PlaylistExtractor = require("../extractor/PlaylistExtractor");
 const { getUrlFromJson } = require("../extractor/LibExtractor");
+const SongDataExtractor = require("../extractor/SongDataExtractor");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -32,11 +33,13 @@ module.exports = {
 
         url = getUrlFromJson(url);
 
+        // CHECKING YOUTUBE LINK
         if (!url.includes('youtube') && !url.includes('youtu.be')) {
             console.log('[COMMAND PLAY] : Invalid URL');
             interaction.reply({content: 'URL invalide !', ephemeral: true});
             return;
         }
+        // CHECKING VOICE CHANNEL CONNECTION
         if (!channel) {
             console.log('[COMMAND PLAY] : User not in a voice channel');
             interaction.reply({content: 'Vous devez être dans un salon vocal !', ephemeral: true});
@@ -46,80 +49,62 @@ module.exports = {
         await interaction.deferReply({ephemeral: false});
 
         let pm = await PlayerManager.getPlayer();
-
         pm.connect(channel);
+
         let song;
 
         // Playlist
         if (url.includes('list=')) {
-            console.log('[COMMAND PLAY] : Playlist detected');
+            let playlist_urls = await PlaylistExtractor.getPlaylistURLs(url);
+            let playlist_data = await SongDataExtractor.extractDetails(url);
 
-            let playlist = await PlaylistExtractor.getPlaylistURLs(url)
-            playlist.forEach((url) => {
-                let s = new Song(url);
-                pm.queue.addToQueue(s);
-            });
-
-            pm.queue.updateQueue();
-
-            if(pm.status !== PlayerManager.STATE.PLAYING) {
-                song = pm.queue.nextSong();
-
-                await pm.playSong(song);
-
-                song.updateSong().then(() => {
-                    pm.song = song;
-                    EmbedManager.getEmbed().update(pm, interaction);
-                });
-                return;
+            if (playlist_urls.length !== playlist_data.length) {
+                throw new Error("Playlist data and playlist urls are not the same length")
             }
+
+            for (let i = 0; i < playlist_urls.length; i++) {
+                const song = new Song(playlist_urls[i]);
+                await song.updateSongExt(playlist_data[i]);
+
+                pm.queue.addToQueue(song);
+            }
+
+            song = pm.queue.nextSong();
+        } else {
+            song = new Song(url);
+            await song.updateSong();
         }
 
-        song = new Song(url);
 
         // When (playing)
         switch (interaction.options.getString('when')) {
-            case 'now':
-                console.log('[COMMAND PLAY] : Play now');
 
-                pm.queue.addToQueueNext(song);
+            case 'now':
                 pm.stopSong();
                 await pm.playSong(song);
 
-                song.updateSong().then(() => {
-                    pm.song = song;
-                    EmbedManager.getEmbed().update(pm, interaction);
-                });
+                pm.song = song;
+                await EmbedManager.getEmbed().update(pm, interaction);
 
                 break;
-            case 'next':
-                console.log('[COMMAND PLAY] : Play next');
 
+            case 'next':
                 pm.queue.addToQueueNext(song)
 
-                song.updateSong().then(() => {
-                    interaction.editReply({content: `Ajouté à la file d'attente : ${song.title} - ${song.artist}`});
-                });
+                interaction.editReply({content: `Ajouté à la file d'attente : ${song.title} - ${song.artist}`});
 
                 break;
+
+
             default:
                 if(pm.status !== PlayerManager.STATE.PLAYING) {
-                    console.log('[COMMAND PLAY] : Play last (now)');
                     pm.playSong(song)
+                    pm.song = song;
 
-                    song.updateSong().then(() => {
-                        pm.song = song;
-                        EmbedManager.getEmbed().update(pm, interaction);
-                    });
-
+                    await EmbedManager.getEmbed().update(pm, interaction);
                 } else {
-                    console.log('[COMMAND PLAY] : Play last (queue)');
-
                     pm.queue.addToQueue(song);
-
-                    song.updateSong().then(() => {
-                        interaction.editReply({content: `Ajouté à la file d'attente : ${song.title} - ${song.artist}`});
-                    });
+                    interaction.editReply({content: `Ajouté à la file d'attente : ${song.title} - ${song.artist}`});
                 }
         }
     },
